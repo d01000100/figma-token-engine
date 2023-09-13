c6c5cfe9ca69117f525dcfb9a32878d6f0965c84
Daniel Casado <daniel.casado@frogdesign.com>
2023-09-13 13:37:39 -0600

    Fixes a bug when parsing colors from variables.

--- src/style-dictionary/parsers/variables2json/styleParsing.ts
+++ src/style-dictionary/parsers/variables2json/styleParsing.ts
@@ -1,8 +1,8 @@
 import { Effect } from "figma-js"
-import { TokenType } from "../../types"
-import { parseShadowValue } from "../figmaStylesParser"
+import { ShadowTokenSingleValue, ShadowType, TokenType } from "../../types"
 import { ParsedVariable, ParsingResult, ShadowStyleVariable, TypographyStyleVariable, Variable } from "./types"
 import { getRoute } from "./utils"
+import { rgbaToHex_255 } from "../utils"
 
 function parseTextCaseValue(value: string): string {
   const figma2css: Record<string, string> = {
@@ -90,13 +90,57 @@ function parseTypographyStyle(variable: TypographyStyleVariable): ParsedVariable
     .filter(Boolean) as ParsedVariable[]
 }
 
+
+function parseShadowValue(
+  shadow: Effect & { spread?: number }
+): ShadowTokenSingleValue {
+  /* {
+        "type": "DROP_SHADOW",
+        "visible": true,
+        "color": {
+          "r": 0.7411764860153198,
+          "g": 0.7411764860153198,
+          "b": 0.7411764860153198,
+          "a": 1
+        },
+        "blendMode": "NORMAL",
+        "offset": {
+          "x": 0,
+          "y": 2
+        },
+        "radius": 10,
+        "spread": 2,
+        "showShadowBehindNode": true
+      }
+      */
+  const figmaShadowTypeToParsed: Record<string, ShadowType> = {
+    ['DROP_SHADOW']: ShadowType.dropShadow,
+    ['INNER_SHADOW']: ShadowType.innerShadow,
+  }
+
+  return {
+    color: rgbaToHex_255(shadow.color ?? { r: 0, g: 0, b: 0, a: 0 }),
+    type: figmaShadowTypeToParsed[shadow.type as string],
+    x: shadow.offset?.x ?? 0,
+    y: shadow.offset?.y ?? 0,
+    blur: shadow.radius,
+    spread: shadow.spread ?? 0,
+  }
+}
+
 function parseShadowStyle(variable: ShadowStyleVariable): ParsedVariable {
   const route = getRoute(variable);
   return {
     type: TokenType.boxShadow,
     value: variable.value.effects.map(
-      (shadowValue) =>
-        parseShadowValue(shadowValue as Effect & { spread?: number })
+      (shadowValue) => {
+        try {
+          return parseShadowValue(shadowValue as Effect & { spread?: number })
+        } catch (error) {
+          console.log({variable, shadowValue})
+          throw error;
+        }
+      }
       ),
     route,
   }
